44b50c5823e9faa9c288f263364f0548c5940dc7
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-05-23 18:41:44 -0400

    used wrap_text function in a cool way

--- exercise_7.rb
+++ exercise_7.rb
@@ -2,4 +2,11 @@ def wrap_text(message, decor)
   return decor + message + decor
 end
 
-puts wrap_text("hello", "******")
+first = wrap_text("hello", "###")
+puts first
+
+second = wrap_text(first, "===")
+puts second
+
+third = wrap_text(second, "~~~")
+puts third
